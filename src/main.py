90adff2a5b1b496fd9b9b67ef571c778137b5c93
Andrew Baumann <abaumann@columnit.com>
2016-08-04 17:55:02 -0400

    erorr: file already exists

--- src/main.py
+++ src/main.py
@@ -2,5 +2,14 @@
 @author abaumann 
 '''
 
-import os
+import shutil 
+from glob import glob
 
+starting_dir = raw_input("What is the grandparent directory?")
+dirlist = list()
+dirlist = glob("..\\"+ starting_dir + "*")
+
+print dirlist
+
+for subdirs in dirlist:
+	shutil.copytree(subdirs, "..\\")
